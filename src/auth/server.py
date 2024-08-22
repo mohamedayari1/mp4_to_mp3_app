d8576353a937d8c442622892b779c5970ce176a5
mohamedayari1 <ayarim781@gmail.com>
2024-08-22 16:58:03 +0100

    auth service completed

--- src/auth/server.py
+++ src/auth/server.py
@@ -18,7 +18,60 @@ server.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT')
 def login():
     auth = request.autorization
     if not auth:
-        return {"error": "missing credentials"}, 401
+        return "missing credentials", 401
     
     #check db for username and password
+    cur = mysql.connection.cursor()
+    res =  cur.execute(
+        "SELECT * FROM users WHERE username=%s",(auth.username)
+    )
+    
+    
+    if res > 0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+        
+        if auth.password != password or auth.email != email:
+            return "invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+        
+    else:
+        return "invalid credentials", 404
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers.get("Authorization")
+    
+    if not encoded_jwt:
+        return "missing credentials", 401
+    encoded_jwt = encoded_jwt.split(" ")[1]
+    
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithms=["HS256"]
+        )
+    except:
+        return decoded, 200
+
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "authz": authz,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+            
+        },
+        secret, 
+        algorithm="HS256"
+    )
+    
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
+    
     
